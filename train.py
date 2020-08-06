445d8741ca12fc69b36c985630f745e1b478f0e6
TobisLee <cn.jlu.lp.5212@gmail.com>
2020-08-06 16:44:02 +0800

    :fire: :zap: Remove test loader in train.py. Add time module.

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import json
 import argparse
 import os
+import time
 
 import torch
 import torch.nn as nn
@@ -17,7 +18,7 @@ from model.mobilenetv3 import MobileNetV3_Large
 
 
 def train():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #  device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     transform = transforms.Compose([
         transforms.Resize(224),
@@ -25,9 +26,9 @@ def train():
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=4, pin_memory=True)
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=8, pin_memory=True)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=8, pin_memory=True)
+    #  testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    #  testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=8, pin_memory=True)
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
     torch.backends.cudnn.benchmark = True
@@ -42,7 +43,9 @@ def train():
 
     criterion = nn.CrossEntropyLoss()
 
-    for epoch in range(2):
+    start_time = time.time()
+
+    for epoch in range(10):
         total_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             inputs, labels = data[0].cuda(), data[1].cuda()
@@ -59,12 +62,15 @@ def train():
             optimizer.step()
 
             total_loss += loss.item()
-            if i % 20 == 19:
-                print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, total_loss / 20))
+            if i % 10 == 9:
+                print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, total_loss / 10))
                 total_loss = 0.0
+
+    end_time = time.time()
+    print("run time: ", end_time-start_time)
     print('Finished Training...')
 
-    torch.save(net.state_dict(), './checkpoints/mobilenetv3-amp.pth')
+    #  torch.save(net.state_dict(), './checkpoints/mobilenetv3-amp.pth')
 
 
 def main():
